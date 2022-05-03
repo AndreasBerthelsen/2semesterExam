3f85295ad349eda337313f52bfdefc4b387a2957
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-05-03 22:45:36 +0200

    funktions tilstande skabelon rework work in progress

--- src/dk/easv/gui/teacher/controller/NySkabelonMainController.java
+++ src/dk/easv/gui/teacher/controller/NySkabelonMainController.java
@@ -1,16 +1,15 @@
 package dk.easv.gui.teacher.controller;
 
 import dk.easv.gui.teacher.model.CitizenModel;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import org.bouncycastle.crypto.generators.SCrypt;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -46,51 +45,107 @@ public class NySkabelonMainController implements Initializable {
         genScrollPane.setContent(vBox);
     }
 
-    private void setupHelbredTab(){
-        //gen tabs -> gridpane -> header + radiobuttons -> textArea (Radio buttons event handler til inactive textArea)
-        HashMap<Integer, String> tilstandsMap = sM.getHelbredsTilstande();
-        HashMap<Integer, ArrayList<String>> vansklighedsMap= sM.getHelbredVanskligheder();
-
-
-
-    }
-
     private void setupFunkTab() {
         HashMap<Integer, String> tilstandsList = sM.getFunktionsTilstande();
         HashMap<Integer, ArrayList<String>> problemMap = sM.getFunktionsVandskligheder();
-        List<Image> imgList = new ArrayList<>();
-        //5 ikke en god ide, ændre til funktion mængder??
-        for (int i = 0; i <= 5; i++) {
-            Image image = new Image("/dk/easv/Images/funktion" + i + ".png");
-            imgList.add(image);
-        }
-
         int index = 0;
         for (int key : tilstandsList.keySet()) {
             //tab for hver afdeling
-            List<ImageView> imageList = createImageViews(imgList);
             Tab tab = new Tab(tilstandsList.get(key));
-            GridPane gridPane = new GridPane();
 
-
+            VBox contentVBox = new VBox(20);
+            contentVBox.prefHeight(Region.USE_COMPUTED_SIZE);
             for (String string : problemMap.get(key)) {
                 //underpunkter
+                VBox section = new VBox();
+                HBox headerBox = new HBox();
+                Label headerLabel = new Label(string);
+                headerBox.getChildren().add(headerLabel);
+
+                GridPane gridPane = new GridPane();
+                gridPane.setHgap(50);
+                gridPane.setVgap(10);
+                gridPane.addRow(1, new Label("Nuværende Niveau"), new Label("Forventet Niveau"));
+
+                ComboBox<ImageView> currentBox = createNiveauComboBox(); // måske replace med combobox factory
+                ComboBox<String> targetBox = createTargetComboBox(); //Udføre selv | Udfører dele af aktiviteten | Udfører ikke selv aktiviteten | Ikke relevant
+                gridPane.addRow(2, currentBox, targetBox);
+
+                Label boxTitle = new Label("Borgerens Ønsker og mål");
+                TextArea textArea = new TextArea();
+                textArea.setWrapText(true);
+
+                section.getChildren().addAll(headerBox, gridPane, boxTitle, textArea);
+                contentVBox.getChildren().add(section);
+
+
+                //TODO FIX SCROLLPANE SCROLL???
+                ScrollPane scrollPane = new ScrollPane(contentVBox);
+                scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+                scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+                scrollPane.prefHeight(11140);
+                scrollPane.prefWidth(5000);
+                tab.setContent(scrollPane);
             }
             funktionInnerTabPane.getTabs().add(tab);
         }
 
     }
 
-    private ArrayList<ImageView> createImageViews(List<Image> imageList) {
-        ArrayList<ImageView> list = new ArrayList<>();
-        for (Image i : imageList) {
-            ImageView imageView = new ImageView(i);
-            list.add(imageView);
+    private ComboBox<String> createTargetComboBox() {
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.getItems().add("Udføre selv");
+        comboBox.getItems().add("Udfører dele af aktiviteten");
+        comboBox.getItems().add("Udfører ikke selv aktiviteten");
+        comboBox.getItems().add("Ikke relevant");
+        return comboBox;
+    }
+
+    private ComboBox<ImageView> createNiveauComboBox() {
+        List<ImageView> imgViewList = new ArrayList<>();
+        for(Image image : createImages()){
+            imgViewList.add(new ImageView(image));
         }
-        return list;
+        ComboBox<ImageView> comboBox = new ComboBox<>(
+                FXCollections.observableArrayList(imgViewList)
+        );
+        //TODO STOP MED A LOADE ALT FOR MANGE BILLDER APP TAGER 3 SEC OM AT ÅBNE
+        //TODO update databasen med nye textfelt + forventet status
+        comboBox.setButtonCell(new ListCell<>() {
+                                   @Override
+                                   protected void updateItem(ImageView item, boolean empty) {
+                                       super.updateItem(item, empty);
+                                       if (item == null || empty) {
+                                           setGraphic(null);
+                                       } else {
+                                           setGraphic(new ImageView(item.getImage()));
+                                       }
+                                   }
+                               }
+        );
+
+
+        return comboBox;
+    }
+
+    private void setupHelbredTab() {
+        //gen tabs -> gridpane -> header + radiobuttons -> textArea (Radio buttons event handler til inactive textArea)
+        HashMap<Integer, String> tilstandsMap = sM.getHelbredsTilstande();
+        HashMap<Integer, ArrayList<String>> vansklighedsMap = sM.getHelbredVanskligheder();
+
+
     }
 
 
+    private ArrayList<Image> createImages() {
+        ArrayList<Image> list = new ArrayList<>();
+        //5 ikke en god ide, ændre til funktion mængder??
+        for (int i = 0; i <= 5; i++) {
+            Image image = new Image("/dk/easv/Images/funktion" + i + ".png");
+           list.add(image);
+        }
+        return list;
+    }
 
 
     public void handleGembtn(ActionEvent actionEvent) {
