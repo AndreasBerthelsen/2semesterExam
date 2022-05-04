745b2994c7fbcfa3cac1ca0ed4de149d3232b59b
MathiasKrarupMadsen <Math249e@easv365.dk>
2022-05-04 12:34:47 +0200

    Add + Edit student gui done

---
+++ src/dk/easv/gui/teacher/controller/AddStudentController.java
@@ -0,0 +1,12 @@
+package dk.easv.gui.teacher.controller;
+
+import javafx.event.ActionEvent;
+
+public class AddStudentController {
+
+    public void handleSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelBtn(ActionEvent actionEvent) {
+    }
+}
---
+++ src/dk/easv/gui/teacher/controller/EditStudentController.java
@@ -0,0 +1,11 @@
+package dk.easv.gui.teacher.controller;
+
+import javafx.event.ActionEvent;
+
+public class EditStudentController {
+    public void handleSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelBtn(ActionEvent actionEvent) {
+    }
+}
