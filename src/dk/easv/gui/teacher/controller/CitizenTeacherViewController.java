c8330cb72ad9d097017647b62e3e6268ad4c3d20
AndreasBerthelsen <andr59j9@easv365.dk>
2022-05-04 18:13:37 +0200

    Citizen WIP

--- src/dk/easv/gui/teacher/controller/CitizenTeacherViewController.java
+++ src/dk/easv/gui/teacher/controller/CitizenTeacherViewController.java
@@ -1,4 +1,20 @@
 package dk.easv.gui.teacher.controller;
 
+import javafx.event.ActionEvent;
+
 public class CitizenTeacherViewController {
+
+    
+
+    public void handleCopyCitizenBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleRemoveCitizenBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleAddCitizenToStudentBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleRemoveCitizenFromStudentBtn(ActionEvent actionEvent) {
+    }
 }
