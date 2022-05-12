cf6da0e1e8b91c3807851b5d8f885c8963b6f813
MathiasKrarup <math249e@easv365.dk>
2022-05-12 14:17:40 +0200

    Admin gui

---
+++ src/dk/easv/gui/admin/controller/AddTeacherOrStudent.java
@@ -0,0 +1,29 @@
+package dk.easv.gui.admin.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddTeacherOrStudent {
+    @FXML
+    private TextField passwordTxtFIeld;
+    @FXML
+    private TextField firstnameTxtField;
+    @FXML
+    private TextField lastnameTxtField;
+    @FXML
+    private TextField usernameTxtField;
+    @FXML
+    private TextField schoolTxtField;
+    @FXML
+    private Button saveBtn;
+    @FXML
+    private Button cancelBtn;
+
+    public void handleSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelBtn(ActionEvent actionEvent) {
+    }
+}
---
+++ src/dk/easv/gui/admin/controller/EditTeacherOrStudent.java
@@ -0,0 +1,27 @@
+package dk.easv.gui.admin.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditTeacherOrStudent {
+    @FXML
+    private TextField firstnameTxtField;
+    @FXML
+    private TextField lastnameTxtField;
+    @FXML
+    private TextField usernameTxtField;
+    @FXML
+    private TextField schoolTxtField1;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private Button saveBtn;
+
+    public void handleSaveBtn(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelBtn(ActionEvent actionEvent) {
+    }
+}
